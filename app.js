be7a90aaea591308531f2cfec0e06c7c453d45b2
TheRootSixtyFour <TheRootSixtyFour@gmail.com>
2023-01-09 15:34:05 +0100

    add image to toggle with light/dark theme

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ switcher.addEventListener('click', function() {
   document.body.classList.toggle('light-theme');
   document.body.classList.toggle('dark-theme');
 
-  const className = document.body.className;
   if(className == "light-theme") {
     this.textContent = "Dark";
+
   } else {
     this.textContent = "Light";
+
   }
 
+  const className = document.body.className;
   console.log('current class name: ' + className);
 });
